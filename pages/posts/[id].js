0bb6b2b4ef5b865106b4f8a02db450d708347661
Alejo <alejob_11@hotmail.com>
2022-03-24 16:37:53 -0500

    NEXTJS-02 - Fix scss import

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Layout from '../../components/layout'
 
-import utilStyles from '../../styles/utils.module.css'
+import utilStyles from '../../styles/utils.module.scss'
 
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
